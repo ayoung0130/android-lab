0d40f910abf43f97379cd59affef1d2e0a949286
ayoung0130 <149597700+ayoung0130@users.noreply.github.com>
2024-01-13 23:08:49 +0900

    0113 ch10

---
+++ ch10_notification/src/main/java/com/android/ch10_notification/ReplyReceiver.kt
@@ -0,0 +1,23 @@
+package com.android.ch10_notification
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.app.RemoteInput
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class ReplyReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        // 알림 입력 글 획득
+        val replyTxt = RemoteInput.getResultsFromIntent(intent)?.getCharSequence("key_text_reply")
+        Log.d("test", "replyTxt : $replyTxt")
+        // 알림 취소
+        val manager = context.getSystemService(
+            AppCompatActivity.NOTIFICATION_SERVICE) as NotificationManager
+        manager.cancel(11)
+    }
+}
