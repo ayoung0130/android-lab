a969a6954f975b60f6be25551dca62172ae03ffa
ayoung0130 <149597700+ayoung0130@users.noreply.github.com>
2024-02-22 18:57:59 +0900

    firebase

--- mediapipe/src/main/java/com/android/mediapipe/MainActivity.kt
+++ mediapipe/src/main/java/com/android/mediapipe/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.android.mediapipe
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.android.mediapipe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        activityMainBinding.navigation.setOnNavigationItemSelectedListener { item ->
+            // Handle navigation item selection
+            when (item.itemId) {
+                R.id.camera_fragment -> {
+                    // Navigate to the CameraFragment
+                    navController.navigate(R.id.camera_fragment)
+                    true
+                }
+                // Ignore other menu items
+                else -> false
+            }
+        }
+
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finish()
     }
 }
