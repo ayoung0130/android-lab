a8ad3334fd939ab20c9395d2440d4e1a8e8a4b12
ayoung0130 <149597700+ayoung0130@users.noreply.github.com>
2024-02-25 20:09:22 +0900

    Flow - MainViewModel

---
+++ flowguide/src/main/java/com/example/flowguide/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.flowguide
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class MainViewModel : ViewModel() {
+
+}
